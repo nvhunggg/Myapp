3cb240fc0fa8f8a36f8e3a8fbe9f0ce0e9e8cd19
nvhunggg <hungnv2@runsystem.net~>
2025-02-11 21:59:26 -0500

    book app

---
+++ src/books.js
@@ -0,0 +1,14 @@
+export const books = [
+    {
+      author: 'Jordan Moore',
+      title: 'Interesting Facts For Curious Minds',
+      img: 'https://images-na.ssl-images-amazon.com/images/I/91I1KDnK1kL._AC_UL600_SR600,400_.jpg',
+      id: 1,
+    },
+    {
+      author: 'James Clear',
+      title: 'Atomic Habits',
+      img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
+      id: 2,
+    },
+  ];
--- src/index.js
+++ src/index.js
@@ -1,17 +1,68 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDom from 'react-dom/client'
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { books } from './books';
+import Book from './Book'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function BookList() {
+  return (
+    <>
+    <h1>amazon best sellers</h1>
+    <section className='booklist'>
+      {/* <EventExamples /> */}
+      {books.map((book, index) => {
+        return <Book {...book} key={book.id} number={index} />;
+      })}
+    </section>
+    </>
+  );
+}
+const EventExamples = () => {
+  const handleFormInput = (e) => {
+    console.log(e);
+    // e.target - element
+    console.log(`Input Name : ${e.target.name}`);
+    console.log(`Input Value : ${e.target.value}`);
+    console.log('handle form input');
+  };
+  const handleButtonClick = () => {
+    alert('handle button click');
+  };
+  const handleFormSubmission = (e) => {
+    e.preventDefault();
+    console.log('form submitted');
+  };
+  return (
+    <section>
+      <form onSubmit={handleFormSubmission}>
+        <h2>Typical Form</h2>
+        <input
+          type='text'
+          name='example'
+          onChange={handleFormInput}
+          style={{ margin: '1rem 0' }}
+        />
+         <button type='submit'>submit form</button>
+      </form>
+      <button onClick={handleButtonClick}>click me</button>
+    </section>
+  );
+};
+
+// const Book = (img, title, author) => {
+//     console.log(props);
+//     const {img, title, author} = props
+//      return <article className='book'>
+//          <img src={img} alt={props.title}/>
+//          <h2>{title.toUpperCase()}</h2>
+//          <h4 style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.5rem' }}>{author}</h4>
+         
+//      </article>
+//  }
+
+
+const root = ReactDom.createRoot(document.getElementById('root'));
+
+root.render(<BookList/>)
